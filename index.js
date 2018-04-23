20ff5d32ddfc347513b2d60bb8b880081ade9c21
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-23 22:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(-2);
 };
 
-const selectingDrivers = [returnFirstTwoDrivers];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
